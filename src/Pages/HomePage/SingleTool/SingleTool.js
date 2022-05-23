60b54fd4ffa50513455eb9d16e8ffd6712c11adf
Your Name <you@example.com>
2022-05-23 04:39:35 +0300

    purchase page done

--- src/Pages/HomePage/SingleTool/SingleTool.js
+++ src/Pages/HomePage/SingleTool/SingleTool.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
@@ -26,7 +27,66 @@ const SingleTool = () => {
     }
 
     const { name, availableQuantity, minimumQuantity, price, picture, description } = serviceDetail;
+    console.log(parseInt(availableQuantity), parseInt(minimumQuantity))
 
+    const handlePurchase = event =>{
+        event.preventDefault();
+        const address = event.target.address.value;
+        const phone = event.target.phone.value;
+        const quantity = event.target.quantity.value;
+        console.log(user.displayName)
+
+        const quantityInInt = parseInt(quantity);
+        const availableQuantityInInt = parseInt(availableQuantity);
+        const minimumQuantityInInt = parseInt(minimumQuantity);
+        const total = quantityInInt * price;
+        event.target.reset();
+
+        if ((minimumQuantityInInt <= quantityInInt) && (quantityInInt <= availableQuantityInInt)){
+           
+            const bookedTool = {
+                name: name,
+                price: price,
+                userName: user.displayName,
+                email: user.email,
+                phone: phone,
+                address : address,
+                quantity : quantity, 
+                total: total
+            }
+            console.log(bookedTool);
+
+            const url = `http://localhost:5000/booked`;
+
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(bookedTool)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.success) {
+                        toast(`${name} is booked`)
+                    }
+                    else {
+                        toast.error(`something went wrong. Please try again`)
+                    }
+                    
+    
+                })
+
+
+
+        }
+        else{
+            toast(`Your quantity must be less than ${availableQuantity} and more than ${minimumQuantity} `)
+        }
+
+        
+    } 
 
 
     return (
@@ -34,7 +94,7 @@ const SingleTool = () => {
             <figure className='bg-primary'><img className='w-96 ' src={picture} alt="Album" /></figure>
             <div class="card-body bg-secondary lg:w-1/3">
                 <h2 className="card-title text-primary justify-center mb-6 text-3xl">Purchase Information</h2>
-                <form /* onSubmit={handleBooking} */ className='grid grid-cols-1 gap-4 justify-items-center'>
+                <form  onSubmit={handlePurchase}  className='grid grid-cols-1 gap-4 justify-items-center'>
                     <div class="form-control w-full max-w-xs">
                         <label class="label">
                             <span class="label-text">Name</span>
@@ -51,13 +111,13 @@ const SingleTool = () => {
                         <label class="label">
                             <span class="label-text">Address</span>
                         </label>
-                        <input type="text"  name='address' placeholder="Address" className="input input-bordered w-full max-w-xs text-lg font-bold" />
+                        <input type="text"  name='address' placeholder="Address" className="input input-bordered w-full max-w-xs text-lg font-bold" required />
                     </div>
                     <div class="form-control w-full max-w-xs">
                         <label class="label">
                             <span class="label-text">Phone No.</span>
                         </label>
-                        <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs text-lg font-bold" />
+                        <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs text-lg font-bold" required/>
                     </div>
                     <div class="form-control w-full max-w-xs">
                         <label class="label">
@@ -69,13 +129,13 @@ const SingleTool = () => {
                         <label class="label">
                             <span class="label-text">Quantity</span>
                         </label>
-                        <input type="number" id='quantity' name='quantity' className="input input-bordered w-full max-w-xs text-lg font-bold" />
+                        <input type="number" id='quantity' name='quantity' className="input input-bordered w-full max-w-xs text-lg font-bold" required />
                     </div>
                     <div class="form-control w-full max-w-xs">
                         <label class="label">
                             <span class="label-text">Price</span>
                         </label>
-                        <input type="number" id='price' name='quantity' disabled value={price} className="input input-bordered w-full max-w-xs text-lg font-bold" />
+                        <input type="number" id='price' name='price' disabled value={price} className="input input-bordered w-full max-w-xs text-lg font-bold" />
                     </div>
 {/*                     <div class="form-control w-full max-w-xs">
                         <label class="label">
