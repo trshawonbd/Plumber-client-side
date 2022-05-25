9bf744fc7ecb0bff13c3ca4197520c4b134ca217
Your Name <you@example.com>
2022-05-25 22:55:05 +0300

    icon

--- src/Pages/HomePage/BusinessSummary/BusinessSummary.js
+++ src/Pages/HomePage/BusinessSummary/BusinessSummary.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import customer from '../../../assets/business/customer.svg';
+import money from '../../../assets/business/money.svg';
+import review from '../../../assets/business/review.svg';
+import tool from '../../../assets/business/tool.svg';
+import view from '../../../assets/business/view.svg';
 
 const BusinessSummary = () => {
     return (
@@ -8,7 +13,8 @@ const BusinessSummary = () => {
 
                 <div className="stat ">
                     <div className="stat-figure text-secondary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+
+                        <img src={customer}  className="inline-block w-12 h-12 stroke-current"  stroke-width="2" alt="" />
                     </div>
                     <div className="stat-title text-2xl ">Customers</div>
                     <div className="stat-value text-secondary">100 + </div>
@@ -16,7 +22,7 @@ const BusinessSummary = () => {
                 </div>
                 <div className="stat ">
                     <div className="stat-figure text-primary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                    <img src={money}  className="inline-block w-12 h-12 stroke-current"  stroke-width="2" alt="" />
                     </div>
                     <div className="stat-title text-2xl">Annual Revenue</div>
                     <div className="stat-value text-primary">120M + </div>
@@ -24,7 +30,7 @@ const BusinessSummary = () => {
                 </div>
                 <div className="stat ">
                     <div className="stat-figure text-secondary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                    <img src={review}  className="inline-block w-12 h-12 stroke-current"  stroke-width="2" alt="" />
                     </div>
                     <div className="stat-title text-2xl">Reviews</div>
                     <div className="stat-value text-secondary">33K + </div>
@@ -32,7 +38,7 @@ const BusinessSummary = () => {
                 </div>
                 <div className="stat ">
                     <div className="stat-figure text-primary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                    <img src={tool}  className="inline-block w-12 h-12 stroke-current"  stroke-width="2" alt="" />
                     </div>
                     <div className="stat-title text-2xl">Tools</div>
                     <div className="stat-value text-primary">50 + </div>
@@ -41,7 +47,7 @@ const BusinessSummary = () => {
 
                 <div className="stat ">
                     <div className="stat-figure text-secondary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                    <img src={view}  className="inline-block w-12 h-12 stroke-current"  stroke-width="2" alt="" />
                     </div>
                     <div className="stat-title text-2xl">Page Views</div>
                     <div className="stat-value text-secondary">2.6M</div>
